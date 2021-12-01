84f05d7e19e864faae9459291350203439e91017
taylor-tokareff <taylortokareff@gmail.com>
2021-12-01 15:30:27 -0800

    initail header details created and rendering

---
+++ src/components/HeaderDetail.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const HeaderDetail = () => (
+  <div>
+    <h1>Taylor Tokareff</h1>
+  </div>
+);
+
+
+
+
+export default HeaderDetail;
